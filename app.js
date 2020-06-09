ea527c8d2e2a9945cdd074ec08a8c6bd717a84ab
Dhruv Seth <dhruvseth@Dhruvs-MacBook-Pro.local>
2020-06-10 05:28:23 +0630

    Stage for deployment

--- app.js
+++ app.js
@@ -110,6 +110,7 @@ app.post("/deleteCustomList", (req, res) => {
     }).then(() => res.redirect("/"));
 })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running on port 3000");
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 })
